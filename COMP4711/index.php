293ab3260ff2173ef6313c444f97d1799f53a492
Sam Salvail <ssalvail39@gmail.com>
2016-01-06 11:04:36 -0800

    strings

--- COMP4711/index.php
+++ COMP4711/index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-/**
+<!--/**
  * Created by PhpStorm.
  * User: Sam Salvail
  * Date: 2016-01-06
  * Time: 10:52 AM
- */
+ */-->
 <html>
     <head>
         <title>index</title>
@@ -12,7 +12,16 @@
     <body>
     <?php
         //code
-        echo"hello world";
+        $temp = 'Jim ';
+        echo 'Hi, my name is ';
+        echo $temp;
+        $temp = 'geek ';
+        echo "I am a ";
+        echo $temp;
+        $temp = 10;
+        echo 'My level is ';
+        echo $temp;
+
     ?>
     </body>
 </html>
