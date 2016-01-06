d87448ddb7183a0a193fd5a27564f983de0bcc29
Sam Salvail <ssalvail39@gmail.com>
2016-01-06 10:56:10 -0800

    index added

---
+++ COMP4711/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+/**
+ * Created by PhpStorm.
+ * User: Sam Salvail
+ * Date: 2016-01-06
+ * Time: 10:52 AM
+ */
+<html>
+    <head>
+        <title>index</title>
+    </head>
+    <body>
+    <?php
+        //code
+        echo"hello world";
+    ?>
+    </body>
+</html>
